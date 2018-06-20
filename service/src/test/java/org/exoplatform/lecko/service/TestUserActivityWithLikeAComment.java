cb8fa38395616a608256aa2e2c59cdc1eb799c07
Romain Dénarié <romain.denarie@exoplatform.com>
2018-06-20 16:50:54 +0200

    add TU for comment a comment and like a comment

--- service/src/test/java/org/exoplatform/lecko/service/TestUserActivityWithLikeAComment.java
+++ service/src/test/java/org/exoplatform/lecko/service/TestUserActivityWithLikeAComment.java
@@ -1,16 +1,5 @@
 package org.exoplatform.lecko.service;
 
-import org.exoplatform.addons.lecko.LeckoServiceController;
-import org.exoplatform.addons.lecko.SimpleDataBuilder;
-import org.exoplatform.lecko.test.AbstractServiceTest;
-import org.exoplatform.social.core.activity.model.ExoSocialActivity;
-import org.exoplatform.social.core.activity.model.ExoSocialActivityImpl;
-import org.exoplatform.social.core.identity.model.Identity;
-import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
-import org.exoplatform.social.core.identity.provider.SpaceIdentityProvider;
-import org.exoplatform.social.core.space.model.Space;
-import org.junit.Before;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -18,10 +7,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
+
+import org.exoplatform.addons.lecko.LeckoServiceController;
+import org.exoplatform.addons.lecko.SimpleDataBuilder;
+import org.exoplatform.lecko.test.AbstractServiceTest;
+import org.exoplatform.social.core.activity.model.ExoSocialActivity;
+import org.exoplatform.social.core.activity.model.ExoSocialActivityImpl;
+import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
+import org.exoplatform.social.core.space.model.Space;
+
 /**
  * Created by Romain Dénarié (romain.denarie@exoplatform.com) on 24/03/17.
  */
-public class TestUserActivity extends AbstractServiceTest {
+public class TestUserActivityWithLikeAComment extends AbstractServiceTest {
 
   private List<Space> tearDown         = new ArrayList<Space>();
 
@@ -61,21 +61,24 @@ public class TestUserActivity extends AbstractServiceTest {
     comment.setTitle("Mary's Comment");
     comment.setUserId(maryIdentity.getId());
     activityManager.saveComment(activity, comment);
+    activityManager.saveLike(comment, jackIdentity);
 
-    activityManager.saveLike(activity, maryIdentity);
 
     // Jack comments activity
     ExoSocialActivity commentJack = new ExoSocialActivityImpl();
     commentJack.setTitle("Jack's Comment");
     commentJack.setUserId(jackIdentity.getId());
+    commentJack.setParentCommentId(comment.getId());
     activityManager.saveComment(activity, commentJack);
+    activityManager.saveLike(commentJack, maryIdentity);
+
 
 
 
 
   }
 
-  public void testBuildUserExport() throws Exception {
+  public void testBuildUserExportWithLikeAComment() throws Exception {
 
     String extractOutputPath = LeckoServiceController.getRootPath() + "/" + LeckoServiceController.getFileName();
     File file = new File(extractOutputPath);
@@ -107,17 +110,24 @@ public class TestUserActivity extends AbstractServiceTest {
     assertEquals("comment", line2[1]);
     assertTrue(line2.length==3);
 
-    // 3;comment;2017-03-30T10:12:36.773+02:00;;
+    // 3;like;2017-03-30T10:12:36.743+02:00;;
     String[] line3 = lines[2].split(";");
     assertTrue(isInteger(line3[0]));
-    assertEquals("comment", line3[1]);
+    assertEquals("like", line3[1]);
     assertTrue(line3.length==3);
 
-    // 4;like;2017-03-30T10:12:36.743+02:00;;
+
+    // 4;comment;2017-03-30T10:12:36.773+02:00;;
     String[] line4 = lines[3].split(";");
     assertTrue(isInteger(line4[0]));
-    assertEquals("like", line4[1]);
+    assertEquals("comment", line4[1]);
     assertTrue(line4.length==3);
+
+    // 5;like;2017-03-30T10:12:36.743+02:00;;
+    String[] line5 = lines[4].split(";");
+    assertTrue(isInteger(line5[0]));
+    assertEquals("like", line5[1]);
+    assertTrue(line5.length==3);
 
   }
 
